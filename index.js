7f2f92aa34357474ae3a34196a4004fe419e2bde
swarajgaidhane15 <rutvikgaidhane1509@gmail.com>
2022-11-17 18:14:25 +0530

    Updated index.js to use static images

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ app.use("/posts", postRoutes);
 const MONGO_URI = process.env.MONGOURI;
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+  //*Set static folder up in production
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
